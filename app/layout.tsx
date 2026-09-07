cbbb252d46a90cc2c6fb9f9d0ce08ec3f4a4a794
etienne lee <lfengeti@gmail.com>
2026-09-07 17:00:44 +0800

    Install Supabase Client + Remove BG photo

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,10 +23,6 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
             className={`${anton.variable} ${workSans.variable} h-full antialiased`}
     >
-           <body className="min-h-full flex flex-col relative">
-        <div className="fixed inset-0 -z-10 bg-photo opacity-10 pointer-events-none" />
-        {children}
-      </body>
     </html>
   );
 }
